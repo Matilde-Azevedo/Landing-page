aac3b11c5103f82fe0b69982c62b064d34490d27
Matilde Azevedo <matildeazevedot19@gmail.com>
2026-03-31 17:21:06 +0100

    Adicionando página principal

---
+++ index.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const show = document.querySelector('main.elemento');
+    
+    setTimeout(() =>{
+        show.classList.add('aparecer')
+    }, 15)
+})
